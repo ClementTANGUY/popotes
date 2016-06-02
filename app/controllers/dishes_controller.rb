6795bfa230a9b964c07bdd26478baf4fd4f4b6af
Clément TANGUY <clementtanguy@MacBook-Air-de-Clement.local>
2016-06-02 13:46:25 +0200

    Add Dishes controller, CRUD actions and corresponding routes

---
+++ app/controllers/dishes_controller.rb
@@ -0,0 +1,31 @@
+class DishesController < ApplicationController
+
+  def index
+
+  end
+
+  def new
+
+  end
+
+  def show
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+end
