7d31afd84061daa4cc145d45341b1c49ec73da2a
ctanguy <clemdelatang@gmail.com>
2018-11-06 20:17:57 +0100

    Fix JS Google Autocomplete

--- app/assets/javascripts/google_autocomplete.js
+++ app/assets/javascripts/google_autocomplete.js
@@ -5,64 +5,27 @@
     var components = getAddressComponents(place);
 
     var cookAddressOne = document.getElementById('cook_cook_places_attributes_0_place_attributes_full_address');
-    var cookAddressTwo = document.getElementById('cook_cook_places_attributes_1_place_attributes_full_address');
-    var cookAddressThree = document.getElementById('cook_cook_places_attributes_2_place_attributes_full_address');
-
     cookAddressOne.blur();
-    cookAddressTwo.blur();
-    cookAddressThree.blur();
-
     cookAddressOne.value = components.full_address;
-    cookAddressTwo.value = components.full_address;
-    cookAddressThree.value = components.full_address;
 
     document.getElementById('cook_cook_places_attributes_0_place_attributes_zip_code').value = components.zip_code;
     document.getElementById('cook_cook_places_attributes_0_place_attributes_city').value = components.city;
 
+    var cookAddressTwo = document.getElementById('cook_cook_places_attributes_1_place_attributes_full_address');
+    cookAddressTwo.blur();
+    cookAddressTwo.value = components.full_address;
+
     document.getElementById('cook_cook_places_attributes_1_place_attributes_zip_code').value = components.zip_code;
     document.getElementById('cook_cook_places_attributes_1_place_attributes_city').value = components.city;
+
+    var cookAddressThree = document.getElementById('cook_cook_places_attributes_2_place_attributes_full_address');
+    cookAddressThree.blur();
+    cookAddressThree.value = components.full_address;
 
     document.getElementById('cook_cook_places_attributes_2_place_attributes_zip_code').value = components.zip_code;
     document.getElementById('cook_cook_places_attributes_2_place_attributes_city').value = components.city;
 
   }
-
-  document.addEventListener("DOMContentLoaded", function() {
-    var cookAddressOne = document.getElementById('cook_cook_places_attributes_0_place_attributes_full_address');
-    var cookAddressTwo = document.getElementById('cook_cook_places_attributes_1_place_attributes_full_address');
-    var cookAddressThree = document.getElementById('cook_cook_places_attributes_2_place_attributes_full_address');
-
-    if (cookAddressOne) {
-      var autocomplete = new google.maps.places.Autocomplete(cookAddressOne, { types: ['geocode'], componentRestrictions: {country: 'fr'} });
-      google.maps.event.addListener(autocomplete, 'place_changed', onPlaceChanged);
-      google.maps.event.addDomListener(cookAddressOne, 'keydown', function(e) {
-        if (e.key === "Enter") {
-          e.preventDefault(); // Do not submit the form on Enter.
-        }
-      });
-    }
-
-    if (cookAddressTwo) {
-      var autocomplete = new google.maps.places.Autocomplete(cookAddressTwo, { types: ['geocode'], componentRestrictions: {country: 'fr'} });
-      google.maps.event.addListener(autocomplete, 'place_changed', onPlaceChanged);
-      google.maps.event.addDomListener(cookAddressTwo, 'keydown', function(e) {
-        if (e.key === "Enter") {
-          e.preventDefault(); // Do not submit the form on Enter.
-        }
-      });
-    }
-
-    if (cookAddressThree) {
-      var autocomplete = new google.maps.places.Autocomplete(cookAddressThree, { types: ['geocode'], componentRestrictions: {country: 'fr'} });
-      google.maps.event.addListener(autocomplete, 'place_changed', onPlaceChanged);
-      google.maps.event.addDomListener(cookAddressThree, 'keydown', function(e) {
-        if (e.key === "Enter") {
-          e.preventDefault(); // Do not submit the form on Enter.
-        }
-      });
-    }
-
-  });
 
   function getAddressComponents(place) {
     // If you want lat/lng, you can look at:
@@ -104,4 +67,42 @@
       city: city
     };
   }
+
+  document.addEventListener("DOMContentLoaded", function() {
+    var cookAddressOne = document.getElementById('cook_cook_places_attributes_0_place_attributes_full_address');
+    var cookAddressTwo = document.getElementById('cook_cook_places_attributes_1_place_attributes_full_address');
+    var cookAddressThree = document.getElementById('cook_cook_places_attributes_2_place_attributes_full_address');
+
+    if (cookAddressOne) {
+      var autocomplete = new google.maps.places.Autocomplete(cookAddressOne, { types: ['geocode'], componentRestrictions: {country: 'fr'} });
+      google.maps.event.addListener(autocomplete, 'place_changed', onPlaceChanged);
+      google.maps.event.addDomListener(cookAddressOne, 'keydown', function(e) {
+        if (e.key === "Enter") {
+          e.preventDefault(); // Do not submit the form on Enter.
+        }
+      });
+    }
+
+    if (cookAddressTwo) {
+      var autocomplete = new google.maps.places.Autocomplete(cookAddressTwo, { types: ['geocode'], componentRestrictions: {country: 'fr'} });
+      google.maps.event.addListener(autocomplete, 'place_changed', onPlaceChanged);
+      google.maps.event.addDomListener(cookAddressTwo, 'keydown', function(e) {
+        if (e.key === "Enter") {
+          e.preventDefault(); // Do not submit the form on Enter.
+        }
+      });
+    }
+
+    if (cookAddressThree) {
+      var autocomplete = new google.maps.places.Autocomplete(cookAddressThree, { types: ['geocode'], componentRestrictions: {country: 'fr'} });
+      google.maps.event.addListener(autocomplete, 'place_changed', onPlaceChanged);
+      google.maps.event.addDomListener(cookAddressThree, 'keydown', function(e) {
+        if (e.key === "Enter") {
+          e.preventDefault(); // Do not submit the form on Enter.
+        }
+      });
+    }
+
+  });
+
 
